e32e248f710ab2b270170f07a01b119cac3939c3
Justin Morgan <justin.morgan@hey.com>
2021-09-08 11:02:56 -0700

    :wrench: (tailwind) Incorporate colors/fonts/breakpoints into tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '375px',
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+      sans: ['Red Hat Display', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          pale: "hsl(225, 100%, 94%)",
+          bright: "hsl(245, 75%, 52%)"
+        },
+        neutral: {
+          pale: "hsl(225, 100%, 98%)",
+          soft: "hsl(224, 23%, 55%)",
+          dark: "hsl(223, 47%, 23%)"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
